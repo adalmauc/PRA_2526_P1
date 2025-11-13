ac4e6443af565875cf343a4c03120a8c124ebfe2
ADALCHO <adalcho@alumno.upv.es@ldsic-vdi03.upvnet.upv.es>
2025-11-13 14:15:10 +0100

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,94 +1,98 @@
+#ifndef LISTARRAY_H
+#define LISTARRAY_H
+
 #include <iostream>
 #include <stdexcept>
 #include "List.h"
 
 template <typename T>
-class ListArray : public List<T>{
-	private:
-		T* arr;
-		int max;
-		int n;
-		static const int MINSIZE =2;
+class ListArray : public List<T> {
 
-		void resize(int new_size){
-			T* temp = new T[new_size];
-			int j=0;
-			for(int i=0; i<new_size; i++){
-				while(arr[j]==nullptr){
-					j++;
-				}
-				temp[i]=arr[j];
-				j++;
-			}
-			delete[] arr;
-			arr = temp;
-			max = new_size-1;
-		}
+private:
+    T* arr;
+    int max;
+    int n;
+    static const int MINSIZE = 2;
 
-	public:
-		void ListArray(){
-			arr = new T[MINSIZE];
-			max = MINSIZE;
-			n = 0;
-		}
+    void resize(int new_size) {
+        T* new_arr = new T[new_size];
+        for (int i = 0; i < n; i++) {
+            new_arr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = new_arr;
+        max = new_size;
+    }
 
-		T operator[](int pos){
-			if(pos<0 || pos >= n){
-				throw(std::out_of_range);
-			}else{
-			return arr[pos];
-			}
-		}
+public:
+    ListArray() {
+        arr = new T[MINSIZE];
+        max = MINSIZE;
+        n = 0;
+    }
 
-		friend std::ostream &operator<<(std::ostream &out, const ListArray<T> &list){
-			for(int i=0; i<n; i++){
-				out << list.arr[i] << " ";
-			}
-			return out;
-		}
+    ~ListArray() {
+        delete[] arr;
+    }
 
-		void insert(int pos, T e) override {
-        if (pos < 0 || pos > n) throw std::out_of_range("posicion invalida");
+    void insert(int pos, T e) override {
+        if (pos < 0 || pos > n) throw std::out_of_range("Posición inválida");
         if (n == max) resize(max * 2);
         for (int i = n; i > pos; i--) arr[i] = arr[i - 1];
         arr[pos] = e;
         n++;
-		}
+    }
 
-		void append(T e) override {
+    void append(T e) override {
         insert(n, e);
-		}
+    }
 
-		void prepend(T e) override {
+    void prepend(T e) override {
         insert(0, e);
-		}
+    }
 
-		T remove(int pos) override {
-        if (pos < 0 || pos >= n) throw std::out_of_range("posicion invalida");
+    T remove(int pos) override {
+        if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
         T elem = arr[pos];
         for (int i = pos; i < n - 1; i++) arr[i] = arr[i + 1];
         n--;
         if (n < max / 4 && max / 2 >= MINSIZE) resize(max / 2);
         return elem;
-		}
+    }
 
-		T get(int pos) override {
+    T get(int pos) override {
         if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
         return arr[pos];
-		}
+    }
 
-		int search(T e) override {
+    int search(T e) override {
         for (int i = 0; i < n; i++) {
             if (arr[i] == e) return i;
         }
         return -1;
-		}
+    }
 
-		bool empty() override {
+    bool empty() override {
         return n == 0;
-		}
+    }
 
-		int size() override {
+    int size() override {
         return n;
-		}
-}
+    }
+
+    T operator[](int pos) {
+        if (pos < 0 || pos >= n) throw std::out_of_range("Posición inválida");
+        return arr[pos];
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const ListArray<T>& list) {
+        out << "List => [";
+        for (int i = 0; i < list.n; i++) {
+            out << "\n  " << list.arr[i];
+        }
+        out << "\n]";
+        return out;
+    }
+};
+
+#endif
